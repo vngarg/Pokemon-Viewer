d871f2297b65c4be1ce9739186a2e203a090c226
vngarg <sngargrsd@gmail.com>
2020-03-28 01:36:09 +0530

    Done

--- lib/PokemonDetail.dart
+++ lib/PokemonDetail.dart
@@ -10,42 +10,59 @@ class PokeDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     bodyWidget() => Stack(
           children: <Widget>[
-            Container(
+            Positioned(
+              height: MediaQuery.of(context).size.height/1.5,
+              width: MediaQuery.of(context).size.width - 20,
+              left:10.0,
+              top: MediaQuery.of(context).size.height * 0.1,
               child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0)
+                ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Text(pokemon.name),
+
+                    SizedBox(height: 100.0,),
+                    
+                    Text(pokemon.name , style: TextStyle(fontSize: 22.0 , fontWeight: FontWeight.bold),),
                     Text('Height: ${pokemon.height}'),
                     Text('Weight: ${pokemon.weight}'),
-                    Text('Types'),
+                    Text('Types' , style: TextStyle(fontWeight: FontWeight.bold),),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: pokemon.type
                           .map((t) => FilterChip(
-                            backgroundColor: Colors.amber,
+                                backgroundColor: Colors.amber,
                                 label: Text(t),
                                 onSelected: (b) {},
                               ))
                           .toList(),
                     ),
-                    Text('Weakness'),
+                    Text('Weakness' , style: TextStyle(fontWeight: FontWeight.bold),),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: pokemon.weaknesses
                           .map((t) => FilterChip(
-                            backgroundColor: Colors.red,
-                                label: Text(t , style: TextStyle(color: Colors.white),),
+                                backgroundColor: Colors.red,
+                                label: Text(
+                                  t,
+                                  style: TextStyle(color: Colors.white),
+                                ),
                                 onSelected: (b) {},
                               ))
                           .toList(),
                     ),
-                    Text('Next Evolution'),
+                    Text('Next Evolution' , style: TextStyle(fontWeight: FontWeight.bold),),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: pokemon.nextEvolution
                           .map((n) => FilterChip(
-                            backgroundColor: Colors.green,
-                                label: Text(n.name , style: TextStyle(color: Colors.white),),
+                                backgroundColor: Colors.green,
+                                label: Text(
+                                  n.name,
+                                  style: TextStyle(color: Colors.white),
+                                ),
                                 onSelected: (b) {},
                               ))
                           .toList(),
@@ -53,6 +70,19 @@ class PokeDetail extends StatelessWidget {
                   ],
                 ),
               ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Hero(tag: pokemon.img , child: Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(pokemon.img)
+                  )
+                ),
+              ),),
             )
           ],
         );
